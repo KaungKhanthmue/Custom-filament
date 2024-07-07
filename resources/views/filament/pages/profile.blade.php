6f45ed23b13a74af43d71bdbacaa1ea4dc1c2d8c
kaungkhanthmue <kaungkhanthmue58@gmail.com>
2024-07-07 21:40:17 +0630

    add profile

--- resources/views/filament/pages/profile.blade.php
+++ resources/views/filament/pages/profile.blade.php
@@ -1,12 +1,26 @@
 <x-filament-panels::page class="2xl:w-[1880px] xl:w-[1250px] lg:w-[1100px]">
     <div class="container mx-auto bg-gray-700 h-[1000px]">
-        <div class=" p-2 w-full  h-[420px]">
+        <div class=" p-2 w-full  bg-gray-600">
             <img src="https://tailwindcss.com/img/card-top.jpg" alt="" class="w-full h-[400px]">
-            <div class="flex justify-center bg-gray-600">
+            <div class="flex justify-center  border-bottom-1 border-black">
                 <div class="">
                     <div class="font-bold text-4xl pt-[80px]">Kaung Khant Hmue</div>
                     <div class="flex justify-center">(kaung kaung)</div>
                 </div>
+            </div>
+            <div class=" w-full h-[80px] flex justify-between">
+                <div class=" py-4 px-4 ">
+                    <button class="w-[200px] h-[50px] bg-blue-500 rounded-md">Add Story</button>
+                    <button class="w-[200px] h-[50px] bg-gray-500 rounded-md">Edit Profile</button>
+                </div>
+                <div class="py-4 px-4">
+                    <button class="w-[100px] h-[50px] bg-gray-500 rounded-md">...</button>
+                </div>
+            </div>
+            <div class=" w-full h-[40px] flex px-4">
+                <button class="w-[100px] h-[40px] bg-blue-200 bg-opacity-25 rounded-xl text-black ml-2">Post</button>
+                <button class="w-[100px] h-[40px]  text-black ml-2">Photos</button>
+                <button class="w-[100px] h-[40px]  text-black ml-2">Videos</button>
             </div>
         </div>
         <div class="bg-black w-[300px] h-[300px] rounded-full z-10 absolute top-[350px] left-[42%] "></div>
